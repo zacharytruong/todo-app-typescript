36f22b6c4c14872ff0588906a0eb8966ce558dea
Zachary Truong <zacharytruong@gmail.com>
2022-07-19 15:06:09 -0500

    completed simple todo with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,53 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 } from 'uuid'
+type Task = {
+  id: string, 
+  title: string, 
+  completed: boolean, 
+  createdAt: Date
+}
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = loadTask();
+tasks.forEach(task => addListItem(task))
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-import confetti from 'canvas-confetti';
+  if (input?.value == null || input?.value === '') return;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+  const task: Task = {
+    id: v4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+  tasks.push(task)
+  addListItem(task)
+  input.value = ''
+})
+
+
+function addListItem(task: Task) {
+  const item = document.createElement('li')
+  const label = document.createElement('label')
+  const checkbox = document.createElement('input')
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked
+  })
+  saveTask();
+  checkbox.type = 'checkbox'
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function saveTask() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks))
+}
+
+function loadTask(): Task[] {
+  const tasksJSON = localStorage.getItem('TASKS');
+  if (tasksJSON == null) return [];
+  return JSON.parse(tasksJSON)
+}
